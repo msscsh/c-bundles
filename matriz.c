63dbb3f27406f40245a76d05fee80bd4f6988113
msscsh <itsbruno42@gmail.com>
2023-07-01 21:12:05 -0300

    adjusting indentation and standardization

--- matriz.c
+++ matriz.c
@@ -5,39 +5,43 @@
 
 int main() {
 
-       int m[4][4],i,j,somadalinha3=0;
-       float somadacoluna2=0,somadamatriz=0,Mgeo=1,MG;
+        int m[4][4];
+        int i;
+        int j;
+        int somadalinha3=0;
+        float somadacoluna2=0;
+        float somadamatriz=0;
+        float Mgeo=1;
+        float MG;
        
-       srand(time(NULL));
-       
-       for (j=0; j <=3; j++){
-           for (i=0; i<=3; i++){
-               
-       m[i][j] = (rand()%11+5);
-       somadamatriz += m[i][j] ;
-       if(j == 2 )
-       somadalinha3 += m[i][j] ;
-       if(i == 1 )
-       somadacoluna2 += m[i][j] ;
-       if(i == j )
-       Mgeo = Mgeo * m[i][j] ;
-       
-           }
-           }
-           
-       for (j=0; j <=3; j++){
-           printf("\n");
-           for (i=0; i<=3; i++){
-           printf(" %d |",m[i][j]);
-           }
-           }
-       MG = pow(Mgeo,(1/4));
-       printf("\nSoma da matriz: %.0f\n",somadamatriz);
-       printf("Media da matriz: %.2f\n",somadamatriz/16);
-       printf("Soma da 3a linha: %d\n",somadalinha3);
-       printf("Media da 2a coluna: %.2f\n",somadacoluna2/4);
-       printf("Media geometrica: %.2f\n",MG);
-       
-       getchar();
+        srand(time(NULL));
+
+        for (int j=0; j <=3; j++) {
+            for (int i=0; i<=3; i++) {
+                m[i][j] = (rand()%11+5);
+                somadamatriz += m[i][j];
+                if(j == 2 )
+                somadalinha3 += m[i][j];
+                if(i == 1 )
+                somadacoluna2 += m[i][j];
+                if(i == j )
+                Mgeo = Mgeo * m[i][j];
+            }
+        }
+
+        for (int j=0; j <=3; j++) {
+            printf("\n");
+            for (int i=0; i<=3; i++) {
+                printf(" %d |",m[i][j]);
+            }
+        }
+
+        MG = pow(Mgeo,(1/4));
+
+        printf("\nSoma da matriz: %.0f\n",somadamatriz);
+        printf("Media da matriz: %.2f\n",somadamatriz/16);
+        printf("Soma da 3a linha: %d\n",somadalinha3);
+        printf("Media da 2a coluna: %.2f\n",somadacoluna2/4);
+        printf("Media geometrica: %.2f\n",MG);
 
 }
